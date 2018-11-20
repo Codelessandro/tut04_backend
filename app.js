a9aa82218bf8d5679358f60e295ff70550b5248a
Alessandro Schneider <alessandro.schneider.privat@gmail.com>
2018-11-20 22:21:38 +0100

    ...

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ app.use(function(req, res, next) {
 });
 
 
-
-
 let requests = []
+let urls = []
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/request', function(req, res) {
@@ -27,15 +26,24 @@ app.get('/request', function(req, res) {
 });
 
 app.post('/request', function(req,res) {
-	var message = req.body.message;
-	var name = req.body.name;
-	var email = req.body.email;
-	var title = req.body.title;
-	requests.push({"name" : name, "email" : email, "title" : title, "message" : message})
+	requests.push({"name" : req.body.name, "email" : req.body.email, "title" : req.body.title, "message" : req.body.message})
 	res.send("Thanks for your message.")
 })
 
-let port = 8080;
+
+app.post('/url', function(req,res) {
+	urls.push(req.body.url)
+	res.setHeader('Content-Type', 'application/json');
+    	res.send(JSON.stringify({ url : req.body.url }));
+})
+
+
+app.get('/url', function(req,res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ urls : urls  }));
+})
+
+let port = 3000;
 app.listen(port, function(){
 	console.log("server running")
 	//Something to do when the server starts.
